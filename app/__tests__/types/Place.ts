d0a908690851f572790ad1162c90b326994ea882
sergnio <sergnioa@gmail.com>
2024-01-10 16:26:57 -0600

    add type safety for getPlaces api call and add generic types

---
+++ app/__tests__/types/Place.ts
@@ -0,0 +1,10 @@
+// export a default type
+type Place = {
+  displayName: DisplayName;
+  formattedAddress: string;
+};
+
+type DisplayName = {
+  text: string;
+  languageCode: string;
+};
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export type Nullable<T> = T | null;
+export type Undefinable<T> = T | undefined;
+export type Optional<T> = T | null | undefined;
+
+// convert these all to type
