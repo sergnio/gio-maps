b403aa9ba130ffb05c50dd11d9a0dd1e5fd06eba
sergnio <sergnioa@gmail.com>
2024-01-10 11:36:21 -0600

    add playwright config/example test files

--- app/api/places/route.ts
+++ app/api/places/route.ts
@@ -1,4 +1,9 @@
 export const getPlaces = async () => {
+  const googlePlacesApiKey = process.env.GOOGLE_PLACES_API_KEY;
+  if (!googlePlacesApiKey) {
+    throw new Error("No googlePlacesApiKey was provided! Cannot run quries.");
+  }
+
   try {
     // todo - extract out fetch to a helper function that includes api key on every request
     const res = await fetch(
@@ -11,7 +16,7 @@ export const getPlaces = async () => {
         }),
         headers: {
           "Content-Type": "application/json",
-          "X-Goog-Api-Key": "AIzaSyC9DF-3hUFjQUYHh2gvmPZWkQHH4UcB6pQ",
+          "X-Goog-Api-Key": `${googlePlacesApiKey}`,
           "X-Goog-FieldMask": "places.displayName,places.formattedAddress",
         },
       },
