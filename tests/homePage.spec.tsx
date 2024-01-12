c8c99fba3349af198cabf2a2d0b730344523e52c
sergnio <sergnioa@gmail.com>
2024-01-11 19:37:46 -0600

    add header to check for galactic pizza

--- tests/homePage.spec.tsx
+++ tests/homePage.spec.tsx
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the about page", async ({ page }) => {
+test("should load the home page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto("/");
   // Find an element with the text 'About' and click on it
@@ -13,24 +13,15 @@ test("should navigate to the about page", async ({ page }) => {
   await expect(page).toHaveTitle(/Pizza Finder/i);
 });
 
-/*
-import { test, expect } from '@playwright/test';
+test("should navigate easily to the details page", async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto("/");
+  // Find an element with the text 'Galactic Pizza' and click on it
+  await page.click("text=Galactic Pizza");
+  await expect(page).toHaveURL("details/ChIJe18Tmogn9ocR7UpRi67Wfz8");
+  await expect(
+    page.locator("h1", { hasText: /Galactic Pizza/i }),
+  ).toContainText(/Galactic Pizza/i);
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Pizza Finder/i);
 });
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
- */
