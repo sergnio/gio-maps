3774751471d7d0ee3a44c5215dc779e7eaf3c1ae
sergnio <sergnioa@gmail.com>
2024-01-11 19:30:27 -0600

    add base playwright test

--- tests/homePage.spec.tsx
+++ tests/homePage.spec.tsx
@@ -9,4 +9,28 @@ test("should navigate to the about page", async ({ page }) => {
   await expect(page).toHaveURL("http://localhost:3000/");
   // The new page should contain an h1 with "About"
   await expect(page.locator("h1")).toContainText("GioMaps");
+
+  await expect(page).toHaveTitle(/Pizza Finder/i);
 });
+
+/*
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+ */
